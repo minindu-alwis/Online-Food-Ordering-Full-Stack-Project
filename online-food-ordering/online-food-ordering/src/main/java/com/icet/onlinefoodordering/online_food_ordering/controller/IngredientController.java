b502d5a884cbfce87f451095c15ca7be9a1b8f81
minindu-alwis <minindualwis@gmail.com>
2025-03-17 22:48:25 +0530

    Ingredient and category apis are redy

--- online-food-ordering/online-food-ordering/src/main/java/com/icet/onlinefoodordering/online_food_ordering/controller/IngredientController.java
+++ online-food-ordering/online-food-ordering/src/main/java/com/icet/onlinefoodordering/online_food_ordering/controller/IngredientController.java
@@ -1,8 +1,16 @@
 package com.icet.onlinefoodordering.online_food_ordering.controller;
 
+import com.icet.onlinefoodordering.online_food_ordering.model.IngredientCategory;
+import com.icet.onlinefoodordering.online_food_ordering.model.IngredientsItem;
+import com.icet.onlinefoodordering.online_food_ordering.request.IngredientCategoryRequest;
+import com.icet.onlinefoodordering.online_food_ordering.request.IngredientRequest;
+import com.icet.onlinefoodordering.online_food_ordering.service.IngredientsService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/admin/ingredients")
@@ -10,6 +18,45 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class IngredientController {
 
+    private final IngredientsService ingredientsService;
+
+    @PostMapping("/category")
+    public ResponseEntity<IngredientCategory> createIngredientCategory(@RequestBody IngredientCategoryRequest req) throws Exception {
+        IngredientCategory item=ingredientsService.createIngredientCategory(req.getName(),req.getRestaurantId());
+        return new ResponseEntity<>(item, HttpStatus.CREATED);
+
+    }
+
+    @PostMapping()
+    public ResponseEntity<IngredientsItem> createIngredientItem(@RequestBody IngredientRequest req) throws Exception {
+        IngredientsItem item = ingredientsService.createIngredientItem(req.getRestaurantId(), req.getName(), req.getCategoryId());
+        return new ResponseEntity<>(item, HttpStatus.CREATED);
+
+    }
+
+
+    @PutMapping("/{id}/stock")
+    public ResponseEntity<IngredientsItem> updateIngredientStock(@PathVariable Long id) throws Exception {
+        IngredientsItem item = ingredientsService.updateStock(id);
+        return new ResponseEntity<>(item, HttpStatus.OK);
+
+    }
+
+    @GetMapping("/restaurant/{id}")
+    public ResponseEntity<List<IngredientsItem>> getRestaurantIngredient(@PathVariable Long id) throws Exception {
+        List<IngredientsItem> items = ingredientsService.findRestaurantIngredients(id);
+
+        return new ResponseEntity<>(items, HttpStatus.OK);
+
+    }
+
+    @GetMapping("/restaurant/{id}/category")
+    public ResponseEntity<List<IngredientCategory>> getRestaurantIngredientCategory(@PathVariable Long id) throws Exception {
+        List<IngredientCategory> items = ingredientsService.findIngredientCategoryByRestaurantId(id);
+
+        return new ResponseEntity<>(items, HttpStatus.OK);
+
+    }
 
 
 }
---
+++ online-food-ordering/online-food-ordering/src/main/java/com/icet/onlinefoodordering/online_food_ordering/request/IngredientRequest.java
@@ -0,0 +1,16 @@
+package com.icet.onlinefoodordering.online_food_ordering.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class IngredientRequest {
+
+    private String name;
+    private Long categoryId;
+    private Long restaurantId;
+
+}
