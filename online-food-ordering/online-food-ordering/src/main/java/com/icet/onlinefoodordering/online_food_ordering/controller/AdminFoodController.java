eeb59a7fdcb375235fc4db20e67afa2f5c1600c3
minindu-alwis <minindualwis@gmail.com>
2025-03-17 16:29:14 +0530

    FOOD API READY

---
+++ online-food-ordering/online-food-ordering/src/main/java/com/icet/onlinefoodordering/online_food_ordering/controller/AdminFoodController.java
@@ -0,0 +1,62 @@
+package com.icet.onlinefoodordering.online_food_ordering.controller;
+
+import com.icet.onlinefoodordering.online_food_ordering.model.Food;
+import com.icet.onlinefoodordering.online_food_ordering.model.Restaurant;
+import com.icet.onlinefoodordering.online_food_ordering.model.User;
+import com.icet.onlinefoodordering.online_food_ordering.request.CreateFoodRequest;
+import com.icet.onlinefoodordering.online_food_ordering.response.MessageResponse;
+import com.icet.onlinefoodordering.online_food_ordering.service.FoodService;
+import com.icet.onlinefoodordering.online_food_ordering.service.RestaurantService;
+import com.icet.onlinefoodordering.online_food_ordering.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/admin/food")
+public class AdminFoodController {
+
+    private final FoodService foodService;
+
+    private UserService userService;
+
+    private RestaurantService restaurantService;
+
+    @PostMapping
+    public ResponseEntity<Food> createFood(@RequestBody CreateFoodRequest req, @RequestHeader("Authorization") String jwt) throws Exception {
+
+        User user=userService.findUserByJwtToken(jwt);
+        Restaurant restaurant=restaurantService.findRestaurantById(req.getRestaurantId());
+        Food food=foodService.createFood(req,req.getCategory(),restaurant);
+
+        return new ResponseEntity<>(food, HttpStatus.CREATED);
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<MessageResponse> deleteFood(@PathVariable Long id, @RequestHeader("Authorization") String jwt) throws Exception {
+
+        User user=userService.findUserByJwtToken(jwt);
+
+        foodService.deleteFood(id);
+        MessageResponse res=new MessageResponse();
+        res.setMessage("Food Deleted Successful");
+
+        return new ResponseEntity<>(res, HttpStatus.CREATED);
+
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Food> updateFoodAvaibilityStatus(@PathVariable Long id, @RequestHeader("Authorization") String jwt) throws Exception {
+
+        User user=userService.findUserByJwtToken(jwt);
+
+        Food food = foodService.updateAvailibilityStatus(id);
+
+
+        return new ResponseEntity<>(food, HttpStatus.CREATED);
+
+    }
+}
---
+++ online-food-ordering/online-food-ordering/src/main/java/com/icet/onlinefoodordering/online_food_ordering/controller/FoodController.java
@@ -0,0 +1,54 @@
+package com.icet.onlinefoodordering.online_food_ordering.controller;
+
+import com.icet.onlinefoodordering.online_food_ordering.model.Food;
+import com.icet.onlinefoodordering.online_food_ordering.model.Restaurant;
+import com.icet.onlinefoodordering.online_food_ordering.model.User;
+import com.icet.onlinefoodordering.online_food_ordering.request.CreateFoodRequest;
+import com.icet.onlinefoodordering.online_food_ordering.service.FoodService;
+import com.icet.onlinefoodordering.online_food_ordering.service.RestaurantService;
+import com.icet.onlinefoodordering.online_food_ordering.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/food")
+public class FoodController {
+
+    private final FoodService foodService;
+
+    private UserService userService;
+
+    private RestaurantService restaurantService;
+
+    @GetMapping("/search")
+    public ResponseEntity<List<Food>> searchFood(@RequestParam String name,@RequestHeader("Authorization") String jwt) throws Exception {
+
+        User user=userService.findUserByJwtToken(jwt);
+        List<Food> foods = foodService.searchFood(name);
+        return new ResponseEntity<>(foods, HttpStatus.CREATED);
+
+    }
+
+    @GetMapping("/restaurant/{restaurantId}")
+    public ResponseEntity<List<Food>> getRestaurantFood(@RequestParam boolean vagetarian,
+                                                        @RequestParam boolean seasonal,
+                                                        @RequestParam boolean nonveg,
+                                                        @RequestParam(required = false) String food_category,
+                                                        @PathVariable Long restaurantId,
+                                                        @RequestHeader("Authorization") String jwt) throws Exception {
+
+        User user=userService.findUserByJwtToken(jwt);
+
+        List<Food> foods = foodService.getRestaurantFood(restaurantId,vagetarian,nonveg,seasonal,food_category);
+        return new ResponseEntity<>(foods, HttpStatus.OK);
+
+    }
+
+
+
+}
